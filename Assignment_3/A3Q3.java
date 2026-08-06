318244bc1546340827f2abc6ebbcba287c7087fd
Anshika Tyagi <tyagianshika077@gmail.com>
2026-08-06 23:43:46 +0530

    Create A3Q3.java

---
+++ Assignment_3/A3Q3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class A3Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter Principal Amount: ");
+        double P = sc.nextDouble();
+        
+        System.out.print("Enter Rate of Interest: ");
+        double R = sc.nextDouble();
+        
+        System.out.print("Enter Time (in years): ");
+        double T = sc.nextDouble();
+        
+        double SI = (P * R * T) / 100;
+        
+        System.out.println("\nSimple Interest = " + SI);
+        
+        sc.close();
+    }
+}
